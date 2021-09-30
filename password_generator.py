f1f90e2eccb7b66201216600cce088e802503371
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-09-30 12:29:10 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,120 +1,163 @@
-import secrets
-import random
 import string
-import mysql.connector
+import random
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
 
-store = {}
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    username="root",
-    password="0303",
-    database="test"
-)
-
-mycursor = mydb.cursor()
-
-
-def password_gen(u_name):
-    password = secrets.choice(string.ascii_lowercase)
-    password += secrets.choice(string.ascii_uppercase)
-    password += secrets.choice(string.digits)
-    password += secrets.choice(string.digits)
-    password += secrets.choice(string.hexdigits)
-    password += secrets.choice(string.octdigits)
-    password += secrets.choice(string.octdigits)
-    password += secrets.choice(string.punctuation)
-
-    password = list(password)
-    random.shuffle(password)
-
-    store[u_name] = "".join(password)
-
-    return store[u_name]
+with sqlite3.connect("users.db") as db:
+    cursor = db.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS users(Username TEXT NOT NULL, GeneratedPassword TEXT NOT NULL);")
+cursor.execute("SELECT * FROM users")
+db.commit()
+db.close()
 
 
-def username():
-    user_id = input("Enter User ID : ")
-    user_id = user_id.replace(" ", "_")
+class GUI():
+    def __init__(self, master):
+        self.master = master
+        self.username = StringVar()
+        self.passwordlen = IntVar()
+        self.generatedpassword = StringVar()
+        self.n_username = StringVar()
+        self.n_generatedpassword = StringVar()
+        self.n_passwordlen = IntVar()
 
-    mycursor.execute("select username from storedata;")
-    result = mycursor.fetchall()
+        root.title('Password Generator')
+        root.geometry('660x500')
+        root.resizable(False, False)
 
-    myresult = [x[0] for x in result]
+        self.label = Label(text="Password Generator", anchor=N, fg='darkblue', font='arial 20 bold')
+        self.label.grid(row=0, column=1)
 
-    if (user_id not in myresult) and (user_id not in store):
-        print("Hello {}!, your password is \"{}\"".format(user_id, password_gen(user_id)))
-        print()
+        self.blank_label1 = Label(text="")
+        self.blank_label1.grid(row=1, column=0, columnspan=2)
 
-    else:
-        print("Already Exists!!! Try Again")
-        username()
-    mydb.commit()
+        self.blank_label2 = Label(text="")
+        self.blank_label2.grid(row=2, column=0, columnspan=2)
 
-def authenticate_user():
-    try:
-        sql = "SELECT password FROM storedata WHERE username = %s;"
-        adr = tuple([input("Enter username : ")])
-        mycursor.execute(sql, adr)
-        result = mycursor.fetchone()
-        while len(result):
-            password = input("Enter password : ")
-            if password == result[0]:
-                print("Validation Successful!!")
-                print()
-                return adr
-            else:
-                print("Try again!!")
+        self.blank_label2 = Label(text="")
+        self.blank_label2.grid(row=3, column=0, columnspan=2)
 
-    except:
-        print("Invalid Username")
-        authenticate_user()
+        self.user = Label(text="Enter username: ", font='times 15')
+        self.user.grid(row=4, column=0)
 
-def update():
-    sql = "UPDATE STOREDATA SET PASSWORD = %s WHERE USERNAME = %s;"
-    a = authenticate_user()
-    b = input("Enter Password to Update :")
-    i = (b,a[0])
-    mycursor.execute(sql,i)
-    
-    mydb.commit()
-    
-    print("Password Updated Successfully!")
-    print()
+        self.textfield = Entry(textvariable=self.n_username, font='times 15', bd=6, relief='ridge')
+        self.textfield.grid(row=4, column=1)
+        self.textfield.focus_set()
 
-if __name__ == "__main__":
+        self.blank_label3 = Label(text="")
+        self.blank_label3.grid(row=5, column=0)
 
-    try:
-        # Create Table
-        mycursor.execute("CREATE TABLE STOREDATA (USERNAME VARCHAR (50), PASSWORD VARCHAR(10));")
-    except:
-        # If table already exists, proceed!
-        pass
-    sql = "INSERT INTO STOREDATA (USERNAME, PASSWORD) VALUES (%s, %s);"
-    choice = '_'
-    while choice != '0':
-        if choice in '123':
-            if choice == '1':
-                n = int(input("Enter Number of Users:"))
-                for i in range(n):
-                    # Password Generator function call
-                    username()
-                for i in store.items():
-                    index = tuple(map(str, i))
-                    mycursor.execute(sql, index)
-            if choice == '2':
-                # Authenticate user function call
-                authenticate_user()
-            if choice == '3':
-                update()
+        self.length = Label(text="Enter password length: ", font='times 15')
+        self.length.grid(row=6, column=0)
 
+        self.length_textfield = Entry(textvariable=self.n_passwordlen, font='times 15', bd=6, relief='ridge')
+        self.length_textfield.grid(row=6, column=1)
+
+        self.blank_label4 = Label(text="")
+        self.blank_label4.grid(row=7, column=0)
+
+        self.generated_password = Label(text="Generated password: ", font='times 15')
+        self.generated_password.grid(row=8, column=0)
+
+        self.generated_password_textfield = Entry(textvariable=self.n_generatedpassword, font='times 15', bd=6,
+                                                  relief='ridge', fg='darkgreen')
+        self.generated_password_textfield.grid(row=8, column=1)
+
+        self.blank_label5 = Label(text="")
+        self.blank_label5.grid(row=9, column=0)
+
+        self.blank_label6 = Label(text="")
+        self.blank_label6.grid(row=10, column=0)
+
+        self.generate = Button(text="GENERATE NEW PASSWORD", bd=3, relief='solid', padx=1, pady=1, font='arial 15 bold',
+                               fg='black', bg='white', command=self.generate_pass)
+        self.generate.grid(row=11, column=1)
+
+        self.blank_label5 = Label(text="")
+        self.blank_label5.grid(row=12, column=0)
+
+        self.accept = Button(text="ACCEPT", bd=3, relief='solid', padx=1, pady=1, font='arial 15', fg='darkblue',
+                             bg='white', command=self.accept_fields)
+        self.accept.grid(row=13, column=1)
+
+        self.blank_label1 = Label(text="")
+        self.blank_label1.grid(row=14, column=1)
+
+        self.reset = Button(text="RESET", bd=3, relief='solid', padx=1, pady=1, font='arial 15', fg='darkblue',
+                            bg='white', command=self.reset_fields)
+        self.reset.grid(row=15, column=1)
+
+    def generate_pass(self):
+        upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        lower = "abcdefghijklmnopqrstuvwxyz"
+        chars = "@#%&()\"?!"
+        numbers = "1234567890"
+        upper = list(upper)
+        lower = list(lower)
+        chars = list(chars)
+        numbers = list(numbers)
+        name = self.textfield.get()
+        leng = self.length_textfield.get()
+
+        with sqlite3.connect("users.db") as db:
+            cursor = db.cursor()
+
+        if name == "":
+            messagebox.showerror("Error", "Name cannot be empty")
+            return
+
+        if name.isalpha() == False:
+            messagebox.showerror("Error", "Name must be a string")
+            self.textfield.delete(0, 25)
+            return
+
+        length = int(leng)
+
+        if length < 6:
+            messagebox.showerror("Error", "Password must be atleast 6 characters long")
+            self.textfield.configure(text="")
+            return
         else:
-            print("Welcome, Kindly your choice")
-            print("1. Password Generation")
-            print("2. Password Validation")
-            print("3. Update Password")
-            print("0. Exit")
-        choice = input("Enter Choice : ")
+            self.blank_label1.configure(text="")
 
-    # Commit Database
-    mydb.commit()
+        self.generated_password_textfield.delete(0, length)
+
+        u = random.randint(1, length - 3)
+        l = random.randint(1, length - 2 - u)
+        c = random.randint(1, length - 1 - u - l)
+        n = length - u - l - c
+
+        password = random.sample(upper, u) + \
+                   random.sample(lower, l) + \
+                   random.sample(chars, c) + \
+                   random.sample(numbers, n)
+        random.shuffle(password)
+        gen_passwd = "".join(password)
+        n_generatedpassword = self.generated_password_textfield.insert(0, gen_passwd)
+
+    def accept_fields(self):
+        with sqlite3.connect("users.db") as db:
+            cursor = db.cursor()
+            find_user = ("SELECT * FROM users WHERE Username = ?")
+            cursor.execute(find_user, [(self.n_username.get())])
+
+            if cursor.fetchall():
+                messagebox.showerror("This username already exists!", "Please use an another username")
+            else:
+                insert = str("INSERT INTO users(Username, GeneratedPassword) VALUES(\'%s\', \'%s\');" % (
+                    self.n_username.get(), self.n_generatedpassword.get()))
+                cursor.execute(insert)
+                db.commit()
+                messagebox.showinfo("Success!", "Password generated successfully")
+
+    def reset_fields(self):
+        self.textfield.delete(0, 25)
+        self.length_textfield.delete(0, 25)
+        self.generated_password_textfield.delete(0, 25)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    pass_gen = GUI(root)
+    root.mainloop()
